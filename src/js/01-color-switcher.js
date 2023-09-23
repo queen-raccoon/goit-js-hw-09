d69a39a74275d87dff634e24f3d655fe4ca793b6
queen-raccoon <k.i.d.v.s.k.a.t.2.1.3@gmail.com>
2023-09-24 00:47:42 +0400

    1-color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerColor = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener("click", () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    timerColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    startBtn.disabled = true;
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerColor);
+    startBtn.disabled = false;
+});
